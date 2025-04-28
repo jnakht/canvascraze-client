27bcaaf3dba31ae7a6bc9eb4dc5c3584a29b3d79
jnakht <jisan.cse.cu@gmail.com>
2025-04-28 20:54:51 +0600

    basic styling on all crafts cards

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -1,18 +1,21 @@
 
 
-const GalleryCard = () => {
+const GalleryCard = ({craft}) => {
+    const {itemName, subcategoryName, imageURL, price, rating, customization, processingTime, stockStatus, name, email, shortDescription} = craft;
     return (
         <div className="card lg:card-side bg-base-100 shadow-sm">
-            <figure>
+            <figure className=" w-[30%]">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+                    src={imageURL}
                     alt="Album" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+                <h2 className="card-title">{itemName}</h2>
+                <p><span>Rating: </span>{rating}</p>
+                <p><span>Price: </span>{price}</p>
+                <p><span>Made By:</span> {name}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Listen</button>
+                    <button className="btn btn-primary">View Details</button>
                 </div>
             </div>
         </div>
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -8,7 +8,7 @@ const Gallery = () => {
     return (
         <div>
             <h3>No of Crafts are: {allCrafts.length}</h3>
-            <div>
+            <div className='flex flex-col gap-6 max-w-5/6 mx-auto mt-[76px]'>
                 {
                     allCrafts.map(craft => <GalleryCard craft={craft}></GalleryCard>)
                 }
