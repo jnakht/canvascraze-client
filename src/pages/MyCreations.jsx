f7d3aa5b170af08480f83e51fdf60a60247dc875
jnakht <jisan.cse.cu@gmail.com>
2025-04-28 21:22:34 +0600

    fetch myCreations

--- src/pages/MyCreations.jsx
+++ src/pages/MyCreations.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
+import { authContextHook } from "../utility/AuthHooks";
+import GalleryCard from "../components/GalleryCard";
 
 const MyCreations = () => {
+    const {user} = authContextHook();
+    const [currentUser, setCurrentUser] = useState(user);
+    const [myCreations, setMyCreations] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/arts/${currentUser.email}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setMyCreations(data);
+        })
+    } ,[])
     return (
         <div>
-            my creations
+            <h3>my creations : {currentUser.email}</h3>
+            <h3>Number of Creatins : {myCreations.length}</h3>
+            <div>
+                {
+                    myCreations.map(creation => <GalleryCard craft={creation}></GalleryCard>)
+                }
+            </div>
+
         </div>
     );
 };
