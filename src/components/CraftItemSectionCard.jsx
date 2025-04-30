69ef033a6bb5b9dc7ff119b1e165f0aaf9ad55fd
jnakht <jisan.cse.cu@gmail.com>
2025-04-30 16:44:30 +0600

    crafts item section cards styling with creative button

--- src/components/CraftItemSectionCard.jsx
+++ src/components/CraftItemSectionCard.jsx
@@ -2,18 +2,17 @@
 const CraftItemSectionCard = ({item}) => {
     // const { itemName, subcategoryName, imageURL, price, rating, customization, processingTime, stockStatus, name, email, shortDescription } = card;
     return (
-        <div className=" bg-base-100  h-[513px]   w-full">
-        <figure className="px-10 pt-10">
+        <div className=" bg-base-100  h-[513px] w-full">
+        <figure className="px-10 pt-10  h-[70%]">
             <img
                 src={item?.imageURL}
                 className="rounded-xl" />
         </figure>
-        <div className="card-body items-center text-center">
-            <h2 className="card-title">Card Title</h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-            <div className="card-actions">
-                <button className="btn btn-primary">Buy Now</button>
-            </div>
+        <div className="card-body items-center text-center ">
+            <h2 className="text-[#a55e3f] font-merriweather text-[18px] font-medium tracking-[5px]">BERRIES JAM</h2>
+            <p className="font-lato text-[#58595b] text-[15px] font-light">Home Decor</p>
+            <p className="font-lato text-[#a55e3f] font-bold text-[15px] tracking-[2px]">$29.99</p>
+            <button className="btn bg-[url('https://ik.imagekit.io/ghlgoepam/New%20Folder/1746009359386.png_image.png?updatedAt=1746009458235')] bg-cover bg-no-repeat font-bold text-[15px] w-[191px] h-[50px]">Add To Cart</button>
         </div>
     </div>
     );
