3c9398015e7cf664b2a7d206fdcfedd9d06ce3d9
jnakht <jisan.cse.cu@gmail.com>
2025-04-30 18:04:14 +0600

    crafts item section styling done with even cards styling

--- src/components/CraftItemSectionCard.jsx
+++ src/components/CraftItemSectionCard.jsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
 
 const CraftItemSectionCard = ({item}) => {
     // const { itemName, subcategoryName, imageURL, price, rating, customization, processingTime, stockStatus, name, email, shortDescription } = card;
+    const [hover, setHover] = useState(false);
     return (
-        <div className=" bg-base-100  h-[513px] w-full">
-        <figure className="px-10 pt-10  h-[70%]">
+        <div onMouseOver={() => setHover(true)} 
+        onMouseOut={() => setHover(false)}
+         className=" bg-base-100  h-[513px] w-full">
+        <figure className="px-10 pt-10 relative h-[70%]">
             <img
                 src={item?.imageURL}
                 className="rounded-xl" />
+                 <button className={`btn bg-[url('https://ik.imagekit.io/ghlgoepam/New%20Folder/1746011756191.png_image.png?updatedAt=1746012342488')] bg-center bg-no-repeat font-bold text-[15px] w-[191px] h-[50px] absolute top-37 left-14 text-white border-none transition-opacity duration-300 ease-in ${hover ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>Add To Cart</button>
+                
         </figure>
         <div className="card-body items-center text-center ">
             <h2 className="text-[#a55e3f] font-merriweather text-[18px] font-medium tracking-[5px]">BERRIES JAM</h2>
             <p className="font-lato text-[#58595b] text-[15px] font-light">Home Decor</p>
             <p className="font-lato text-[#a55e3f] font-bold text-[15px] tracking-[2px]">$29.99</p>
-            <button className="btn bg-[url('https://ik.imagekit.io/ghlgoepam/New%20Folder/1746009359386.png_image.png?updatedAt=1746009458235')] bg-cover bg-no-repeat font-bold text-[15px] w-[191px] h-[50px]">Add To Cart</button>
+            
         </div>
+       
     </div>
     );
 };
