9682aa514ff52851b2cd795c6c7a5cf1927b43e2
jnakht <jisan.cse.cu@gmail.com>
2025-05-01 19:56:47 +0600

    working with the newsletter section, not done yet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import Banner from "../components/Banner";
 import CraftsItemSection from "../components/CraftsItemSection";
+import NewsletterSection from "../components/NewsletterSection";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            {/* crafts section */}
             <CraftsItemSection></CraftsItemSection>
-
+            <NewsletterSection></NewsletterSection>
             
         </div>
     );
